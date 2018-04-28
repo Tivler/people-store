4f0e8769ce5183bcfd7b96f397026ec6322fd5b1
Tivler <thomasmckyer@Thomass-MacBook-Pro.local>
2018-04-28 19:53:53 -0400

    Added Javascript and resized the image in the css file

---
+++ app.js
@@ -0,0 +1,9 @@
+const FirstName = document.getElementById('FirstNameField');
+const LastName = document.getElementById('LastNameField');
+const Email = document.getElementById('EmailField');
+const Submit = document.getElementById('SubmitButton');
+
+Submit.addEventListener('click' , function(){
+    console.log("SubmitButtonClicked");
+    
+});
