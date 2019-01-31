303345fc84b93f838ac91a1148d9043ab8437e07
tivler <thomasdmckyer@gmail.com>
2019-01-31 13:39:34 -0500

    deleted the contents of JS file and restructuring form validation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,2 @@
 
 
-const firstName = document.getElementById('inputFirstName');
-const lastName = document.getElementById('inputLastName');
-const email = document.getElementById('inputEmail');
-const submitButton = document.getElementById('submitButton');
-
-
-
-
-/*
-//: You could probably rename the variable above to be a little bit more descriptive like: submitBtn, firstNameField... etc.
-
-Submit.addEventListener('click', function () {
-    let fName = firstName.value,
-        lName = lastName.value,
-        userEmail = email.value;
-
-    // log the value of what has been typed into each field...
-    console.log("SubmitButtonClicked");
-    console.log('fName:', fName);
-});
- */
