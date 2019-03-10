4c950e835c4db3b8ddae0ee9acd1e12730c92bca
tivler <thomasdmckyer@gmail.com>
2019-03-10 15:50:09 -0400

    began working to submit form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,23 @@
-const peopleForm;
+// Goal: We want to extract input from our people form then the user submits it.
+// When the user submits the form we want to create a JavaScript object like the following firstName: 'Tim', lastName: 'McKyer', email: 'tim@tim.{com'}
+
+const signUpForm =  document.querySelector('.form-signup');
+const firstNameField = document.querySelector('.first-name');
+const lasttNameField = document.querySelector('.last-name');
+const emailField = document.querySelector('.email');
+
+function sendData(newUser) {
+    console.log('Creating new user:', newUser);
+}
+
+function submitForm() {
+
+    let newUser = {
+        firstName: firstNameField.value,
+        lastName: lasttNameField.value,
+        email: emailField.value
+    };
+
+    console.log('new user:', newUser);
+
+}
