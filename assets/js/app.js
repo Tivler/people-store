bf6402ed9e858f36c87a2c38d05020f2e8b51349
tivler <thomasdmckyer@gmail.com>
2019-03-10 16:04:02 -0400

    installed body parcer via npm

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 // Goal: We want to extract input from our people form then the user submits it.
 // When the user submits the form we want to create a JavaScript object like the following firstName: 'Tim', lastName: 'McKyer', email: 'tim@tim.{com'}
 
-const signUpForm =  document.querySelector('.form-signup');
+const signUpForm     = document.querySelector('.form-signup');
 const firstNameField = document.querySelector('.first-name');
 const lasttNameField = document.querySelector('.last-name');
-const emailField = document.querySelector('.email');
+const emailField     = document.querySelector('.email');
 
 function sendData(newUser) {
     console.log('Creating new user:', newUser);
 }
 
-function submitForm() {
+function submitForm(e) {
+    e.preventDefault();
 
     let newUser = {
         firstName: firstNameField.value,
