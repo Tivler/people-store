55831f9e7a339e816f9be0f1191b2da5e0a81b2f
tivler <thomasdmckyer@gmail.com>
2019-03-10 16:48:29 -0400

    deleted node modules

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
+const cors       = require('cors')
 
 const app = express();
 
 const port = process.env.PORT || 9090;
 
-app.use(express.static(__dirname + '/'));
+// app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 
+// Allow requests from all domains to hit this API for dev purposes only
+app.use(cors());
 
 // API's
 // Endpoints
 // Where the frontend can send data to the backend
 
+// POST request: http://localhost:9090/api/v1/user
 app.post('api/v1/user/', (request, response) => {
 
     console.log('NEW USER OBJECT:', request.body);
 
 });
 
-app.get('api/health', (request, response) => {
+// GET request: http://localhost:9090/api/health
+app.get('api/health/', (request, response) => {
     response.json({
         status: 200
     });
@@ -28,8 +33,3 @@ app.get('api/health', (request, response) => {
 app.listen(port, () => {
     console.log('App is alive on port:', port);
 });
-
-// Base url for API: http://localhost:9090
-
-// GET request: http://localhost:9090/api/health
-// POST request: http://localhost:9090/api/v1/user
